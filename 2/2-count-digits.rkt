a72cb8ae311c5cf36f0022ee8ca61389628474d1
TeodorTodorov <teodorvulchev@gmail.com>
2018-10-16 14:23:44 +0300

    uu
    u

--- 2/2-count-digits.rkt
+++ 2/2-count-digits.rkt
@@ -3,12 +3,28 @@
 (require rackunit/text-ui)
 
 ; Искаме да преброим цифрите на дадено число
+(define (count-digits x)
+  (define (count-digits2 x)
+  (cond
+    (( =(/ x 10) 0) 0)
+    (( <(/ x 10) 1) 1)
+    (else (+ 1 (count-digits(floor (/ x 10))) ))
+    )
+    )
+  
+  (cond
+    ((< x 0) (count-digits2 (* -1 x)))
+    (else (count-digits2 (* 1 x)))
+    )
+  
+    
+  )
 
 (define tests (test-suite
   "count-digits tests"
 
-  (test-case "single-digit" (check-equal? (count-digits 3) 1))
-  (test-case "many digits" (check-equal? (count-digits 54321) 5))
+  (test-case "single-digit" (check-equal? (count-digits 9) 1))
+  (test-case "many digits" (check-equal? (count-digits -54321) 5))
   (test-case "no-digits" (check-equal? (count-digits 0) 0))
 ))
 
